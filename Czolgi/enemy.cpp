3b323357d3a4fea5837ee800407108d74075a4b7
RoboAsystent <69710714+RoboAsystent@users.noreply.github.com>
2021-04-29 23:45:01 +0200

    Update enemy

--- Czolgi/enemy.cpp
+++ Czolgi/enemy.cpp
@@ -22,24 +22,25 @@ void Enemy::patrol_path(qreal end)
     if(!is_inrange)
     {
 
-    if(y() >= end)
-    {
-        reverso = true;
-        this->setPixmap(QPixmap(":/img/tex/tex_enemy_shadow_bck.png"));
-    }
+        if(y() >= end)
+        {
+            reverso = true;
+            this->setPixmap(QPixmap(":/img/tex/tex_enemy_shadow_bck.png"));
+        }
 
-    if((y()-speed)<= inity)
-    {
-        reverso = false;
-        this->setPixmap(QPixmap(":/img/tex/tex_enemy_shadow_fd.png"));
-    }
+        if((y()-speed)<= inity)
+        {
+            reverso = false;
+            this->setPixmap(QPixmap(":/img/tex/tex_enemy_shadow_fd.png"));
+        }
 
-    if(!reverso)
-        setPos(x(),y()+speed);
+        if(!reverso)
+            setPos(x(),y()+speed);
 
-    else
-        setPos(x(),y()-speed);
-    }
+        else
+            setPos(x(),y()-speed);
+        }
+
     else
     {
         hold_pos();
